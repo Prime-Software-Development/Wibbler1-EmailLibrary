faaf8e5e3862798ecf1cb399a6344dc9027e01cf
chris <chris@a63afa6d-c961-4ff8-9f36-2336277e171c>
2017-04-27 13:14:14 +0000

    Added transformers to change the message prior to sending#

---
+++ trunk/src/transformer/BaseTransformer.php
@@ -0,0 +1,11 @@
+<?php
+namespace Trunk\EmailLibrary\EMail\transformer;
+
+use Trunk\EmailLibrary\EMail\Message;
+
+abstract class BaseTransformer {
+
+	public function transform( Message $message ) {
+
+	}
+}
